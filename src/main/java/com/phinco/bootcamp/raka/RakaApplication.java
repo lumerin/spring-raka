9bedf0c949be6458ef8f20744ac100e0e4d373cb
Raka Aditya <rakaaditya1304@gmail.com>
2023-12-04 11:23:26 +0700

    commit

---
+++ src/main/java/com/phinco/bootcamp/raka/RakaApplication.java
@@ -0,0 +1,13 @@
+package com.phinco.bootcamp.raka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phinco/bootcamp/raka/RakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phinco.bootcamp.raka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
