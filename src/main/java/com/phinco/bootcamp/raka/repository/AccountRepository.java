c8278de897900519d120853dc3a580fe3d5278c9
Raka Aditya <rakaaditya1304@gmail.com>
2023-12-13 11:53:44 +0700

    Fix:fix method getAccountById

--- src/main/java/com/phinco/bootcamp/raka/repository/AccountRepository.java
+++ src/main/java/com/phinco/bootcamp/raka/repository/AccountRepository.java
@@ -1,13 +1,6 @@
 package com.phinco.bootcamp.raka.repository;
 
-import com.phinco.bootcamp.raka.model.Account;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
-
 
 public interface AccountRepository {
-    public Account getAccountById(String id);
+    public Object getAccountById(String id);
 }
--- src/main/java/com/phinco/bootcamp/raka/repository/AccountRepositoryImpl.java
+++ src/main/java/com/phinco/bootcamp/raka/repository/AccountRepositoryImpl.java
@@ -1,17 +1,21 @@
 package com.phinco.bootcamp.raka.repository;
 
 import com.phinco.bootcamp.raka.model.Account;
+import com.phinco.bootcamp.raka.model.AccountRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class AccountRepositoryImpl implements AccountRepository {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+
     @Override
     public Account getAccountById(String id) {
-        String sql = "SELECT * FROM ACCOUNTS WHERE ID = ";
-        return jdbcTemplate.queryForObject(sql, Account.class, id);
+        String sql = "SELECT * FROM accounts WHERE ID = ?";
+        return jdbcTemplate.queryForObject(sql, new AccountRowMapper(), new Object[]{id} );
     }
 }
